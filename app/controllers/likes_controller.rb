fff5d1dafc6815dfe9314bee3c5d799478b57d80
Garvit-Rana <garvitrana2000@gmail.com>
2018-07-04 18:27:43 +0530

    scss added with more features of like and comment

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,19 @@
+class LikesController < ApplicationController
+ before_action :authenticate_user!
+  def toggle
+  
+  #@like=Like.find_by(post_id:params[:post_id],user_id:current_user.id)
+
+@post=Post.find(params[:post_id])
+
+
+if @post.user_can_like(current_user.id)
+
+@like=Like.create(post_id:params[:post_id],user_id:current_user.id)
+
+else
+	Like.find_by(post_id:params[:post_id],user_id:current_user.id).destroy
+end
+redirect_to root_path
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,25 @@
 class Post < ApplicationRecord
 belongs_to :user
 has_many :comments, dependent: :destroy
+has_many :likes
+def user_can_like(user_id)
+	if self.likes.where(user_id:user_id).length>0
+		return false
+      
+	else
+		return true
+
+	end
+
+end
+def like_status_string(user_id)
+	if self.user_can_like(user_id)
+		return 'Like'
+	else
+		return 'Unlike'
+	end
+
+	
+end
+
 end
